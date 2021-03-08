a6cf063a01987de0759bb1ca3bfbc1715b6b3daa
Diego Acosta <diegoacosta211@gmail.com>
2021-03-08 01:40:11 -0300

    memo and useCallback

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ const App = () => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
+  // useCallback wraps a function and memoize it, so none of the dependencies
+  // this functions depends on changes we always get back a cached memoize
+  // version of the function
   const incrementCount1 = useCallback(() => setCount1(count1 + 1), [count1]);
   const incrementCount2 = useCallback(() => setCount2(count2 + 1), [count2]);
 
+  // Memoize the output of the complicated function and makes it reliable on
+  // count1 value
+  // if count1 doesnt change, it will used the cached version of the value
   const doSomethingComplicated = useMemo(() => {
     console.log('I am computing something complex');
     return ((count1 * 1000) % 12.4) * 51000 - 4000;
